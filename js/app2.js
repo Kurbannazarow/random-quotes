da07ccd5d4cf0492c043f56a3cf4b6be49354c28
Eziz <eziz_kurbannazarow@mail.ru>
2020-10-16 01:26:43 +0300

    Added app2 js

---
+++ js/app2.js
@@ -0,0 +1,31 @@
+const quotes = [
+  {
+    qut:
+      "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+    author: " Life"
+  },
+  {
+    qut:
+      "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+    author: "John Kenneth Galbraith"
+  },
+  {
+    qut:
+      "God save me from my friends. I can protect myself from my enemies.",
+    author: "Claude Louis Hector de Villars "
+  },
+  {
+    qut: "The price of anything is the amount of life you exchange for it.",
+    author: "David Thoreau"
+  },
+  {
+    qut:
+      "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+    author: "Charles Lindbergh"
+  },
+  {
+    qut:
+      "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+    author: " Tyne Daly"
+  }
+];
