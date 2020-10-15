468008728c880ed8f416e349c4ae7e6060120d37
Eziz <eziz_kurbannazarow@mail.ru>
2020-10-16 02:16:21 +0300

    Project done

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Random-Quotes-Project
 A small JavaScript project that changes the quote and author on the click of a button.
--- js/app2.js
+++ js/app2.js
@@ -1,17 +1,16 @@
+(function generateQuote() {
+
 const quotes = [
   {
-    qut:
-      "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+    qut: "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
     author: " Life"
   },
   {
-    qut:
-      "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+    qut:  "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
     author: "John Kenneth Galbraith"
   },
   {
-    qut:
-      "God save me from my friends. I can protect myself from my enemies.",
+    qut:  "God save me from my friends. I can protect myself from my enemies.",
     author: "Claude Louis Hector de Villars "
   },
   {
@@ -19,13 +18,25 @@ const quotes = [
     author: "David Thoreau"
   },
   {
-    qut:
-      "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+    qut: "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
     author: "Charles Lindbergh"
   },
   {
-    qut:
-      "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+    qut: "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
     author: " Tyne Daly"
   }
 ];
+
+  const quote = document.querySelector('#quote')
+  const generateBtn = document.querySelector('#generate-btn')
+  const author = document.querySelector('[data-author]')
+
+  generateBtn.addEventListener('click', generate)
+
+  function generate(event) {
+    event.preventDefault()
+    const count = Math.trunc(Math.random() * quotes.length)
+    quote.textContent = quotes[count].qut
+    author.textContent = quotes[count].author
+  }
+})()
